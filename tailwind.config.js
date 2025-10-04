81ad4ec5972c9080f8d88296efd99bf51918de57
Manoj Joshi <manoj.joshi.public@gmail.com>
2025-10-04 10:22:52 +0545

    minor changes on config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#020204",       // darkest background
-        secondary: "#0a1f2d",     // secondary dark background
-        text: "#babab8",          // main text color
+        primary: "#020204", // darkest background
+        secondary: "#0a1f2d", // secondary dark background
+        text: "#babab8", // main text color
         icon: "rgb(26, 86, 126)", // icon color
       },
       fontFamily: {
