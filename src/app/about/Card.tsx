b638378a6292bc57f757c1c33c7317919e8785e8
Manoj Joshi <manoj.joshi.public@gmail.com>
2025-10-04 12:07:53 +0545

    cards updated

--- src/app/about/Card.tsx
+++ src/app/about/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = (props) => {
   return (
-    <div className="bg-gray-200/10 border border-gray-300/20 p-6 rounded-lg shadow-lg text-center w-100 h-auto ">
+    <div className="bg-gray-200/10 border border-gray-300/20 p-6 rounded-lg shadow-lg text-center w-100 h-auto hover:scale-105 transition-transform duration-200">
       <img src={props.icon} alt="Icon" className="mx-auto mb-6 w-16 h-16" />
       <h2 className="text-2xl font-semibold mb-4">{props.title}</h2>
       <p className="text-base text-gray-400">{props.description}</p>
--- src/app/datasets/Card.tsx
+++ src/app/datasets/Card.tsx
@@ -8,7 +8,7 @@ interface CardDProps {
 
 const Card: React.FC<CardDProps> = (props) => {
   return (
-    <div className="bg-gray-200/10 border border-gray-300/20 p-6 rounded-lg shadow-lg w-100 h-auto ">
+    <div className="bg-gray-200/10 border border-gray-300/20 p-6 rounded-lg shadow-lg w-100 h-auto hover:scale-105 transition-transform duration-200">
       <div className="flex">
         <img src={props.icon} alt="Icon" className="mb-6 mr-6 w-auto h-8" />
         <h2 className="text-xl font-semibold mb-4">{props.title}</h2>
