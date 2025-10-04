5453950e019a121224790bbb6c6477bac425851b
Manoj Joshi <manoj.joshi.public@gmail.com>
2025-10-04 12:08:08 +0545

    teams page updated

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,13 +1,15 @@
+import Card from "./Card";
+
 export default function Team() {
   return (
     <div className="min-h-screen bg-primary text-text font-atkinson p-8 sm:p-20">
-      <h1 className="text-4xl font-bold glow mb-6">Meet the Team</h1>
-      <p className="text-lg mb-4">Your team members and their roles go here.</p>
-      <ul className="list-disc list-inside space-y-2">
-        <li>Pradip – Developer / Designer</li>
-        <li>Member 2 – VR / 3D Specialist</li>
-        <li>Member 3 – Data Scientist</li>
-      </ul>
+      <h1 className="text-4xl font-bold glow mb-16 w-full text-center mt-15">Meet the Team</h1>
+      <p className="text-center text-lg mb-16">We build it. We own it. Excellence is non-negotiable. See who drives the mission.</p>
+      <div className="flex items-center justify-center gap-20  w-full ">
+        <Card profile="/profiles/1.jpg" name="John Doe" role="Team Lead" description="Loves coding" linkedin="https://linkedin.com/in/johndoe" github="https://github.com/johndoe" facebook="https://facebook.com/johndoe" />
+        <Card profile="/profiles/1.jpg" name="John Doe" role="UI/UX Designer" description="Loves coding" linkedin="https://linkedin.com/in/johndoe" github="https://github.com/johndoe" facebook="https://facebook.com/johndoe" />
+        <Card profile="/profiles/1.jpg" name="John Doe" role="Data Scientist" description="Loves coding and enthusiast of AI, ML and data science" linkedin="https://linkedin.com/in/johndoe" github="https://github.com/johndoe" facebook="https://facebook.com/johndoe" />
+      </div>
     </div>
   );
 }
