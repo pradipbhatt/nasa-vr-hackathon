8471dcb6cbca29d62d3f3212a836514b60401cbe
Manoj Joshi <manoj.joshi.public@gmail.com>
2025-10-04 08:25:57 +0545

    changes made to about page

---
+++ src/app/about/Card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface CardProps {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+const Card: React.FC<CardProps> = (props) => {
+  return (
+    <div className="bg-gray-200/10 border border-gray-300/20 p-6 rounded-lg shadow-lg text-center w-100 h-auto ">
+      <img src={props.icon} alt="Icon" className="mx-auto mb-6 w-16 h-16" />
+      <h2 className="text-2xl font-semibold mb-4">{props.title}</h2>
+      <p className="text-base text-gray-400">{props.description}</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,18 @@
+import Card from "./Card";
+
 export default function About() {
   return (
     <div className="min-h-screen bg-primary text-text font-atkinson p-8 sm:p-20">
-      <h1 className="text-4xl font-bold glow mb-6">About the Challenge</h1>
-      <p className="text-lg leading-relaxed mb-4">
-        NASA’s open data makes ocean observations accessible to everyone. 
-        This VR experience helps people explore ocean data visually and interactively.
+      <h1 className="text-4xl font-bold glow mb-6 w-full text-center mt-15">About the Challenge</h1>
+      <p className="text-lg leading-relaxed mb-4 text-center">
+        NASA’s open data makes ocean observations accessible to everyone. This VR experience helps people explore ocean data visually and interactively. Our mission is to create an immersive storytelling environment that reveals insights about our oceans, leveraging NASA datasets to connect the
+        audience with the beauty and science of Earth.
       </p>
-      <p className="text-lg leading-relaxed">
-        Your mission is to create an immersive storytelling environment that reveals insights about our oceans, leveraging NASA datasets to connect the audience with the beauty and science of Earth.
-      </p>
+      <div className="mt-12 grid grid-cols-1 sm:grid-cols-3 gap-8">
+        <Card icon="/icons/satellite.svg" title="DATA" description="data is always complex and unintuitive, especially when it comes from satellites" />
+        <Card icon="/icons/VR.svg" title="Experience" description="we created VR storytelling to make this same data intuative and meaningful for normal people" />
+        <Card icon="/icons/earth.svg" title="GOAL" description="we connected normal people with real science sparking curiosity and awareness about nature and the technology behind." />
+      </div>
     </div>
   );
 }
